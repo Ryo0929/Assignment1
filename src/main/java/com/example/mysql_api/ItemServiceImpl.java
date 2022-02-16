5ed65c477ed4179c564af0980a81f5b260457ead
Weitung Liao <>
2022-02-16 09:24:43 -0700

    no message

--- src/main/java/com/example/mysql_api/ItemServiceImpl.java
+++ src/main/java/com/example/mysql_api/ItemServiceImpl.java
@@ -6,8 +6,10 @@ import com.example.mysql_api.mysql_api.db_servicesGrpc;
 import com.example.mysql_api.mysql_api.item;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+@GrpcService
 public class ItemServiceImpl extends db_servicesGrpc.db_servicesImplBase {
     @Autowired
     ItemService itemService;
